3b5a062608ee0d4f7c989ffb3d35b21be0e75f0c
なむな <akegamiyomogi@gmail.com>
2026-03-02 23:24:36 +0900

    入力機能

--- raspberry_garden/lib/routes/application_api.dart
+++ raspberry_garden/lib/routes/application_api.dart
@@ -1,17 +1,23 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 Future<void> sendDiscordMessage({
   required String baseUrl,
   required String apiKey,
+  required String message,
 }) async {
   final uri = Uri.parse('$baseUrl/api/discord/send');
 
   final res = await http.post(
     uri,
     headers: {
+      'Content-Type': 'application/json', // ★必須
       'X-API-KEY': apiKey,
     },
+    body: jsonEncode({
+      'message': message, // ★C#の MessageRequest.Message に対応
+    }),
   );
 
   if (res.statusCode != 200) {
