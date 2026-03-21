54d4dac1b1c99f394be0922ba1ea6171e637d38e
なむな <akegamiyomogi@gmail.com>
2026-03-21 12:10:17 +0900

    Revert "Unity"
    This reverts commit 1c5a30b2f57165fb5efbf6519cf9b9943cdbb991.

---
+++ raspberry_garden/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/splash_page.dart';
+
+void main() {
+  runApp(const AppEntry());
+}
+
+class AppEntry extends StatelessWidget {
+  const AppEntry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
